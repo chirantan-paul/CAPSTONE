68e7b8d135edbeb16f2ee7fecb137df9a28b6810
chirantan-paul <73732001+chirantan-paul@users.noreply.github.com>
2023-02-22 17:03:45 +0530

    Update capstone_app.py

--- capstone_app.py
+++ capstone_app.py
@@ -44,4 +44,4 @@ elif(choice=='Analysis'):
     import altair as alt
     sourc=pd.DataFrame(data_json1['feeds'], columns=['entry_id','field1'])
     st.write(sourc)
-    st.altair_chart(alt.Chart(sourc).mark_line().encode(x='entry_id:O',y='Field1:N'))
+    st.altair_chart(alt.Chart(sourc).mark_line().encode(x='entry_id:O',y='Field1:T'))
