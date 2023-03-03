5e6edde88387437bac3da8334dd51d925028c525
chirantan-paul <73732001+chirantan-paul@users.noreply.github.com>
2023-03-03 21:22:01 +0530

    Update capstone_app.py

--- capstone_app.py
+++ capstone_app.py
@@ -19,27 +19,39 @@ st.title("Welcome To Our Boost Converter Monitoring Online UI")
 menu = ["Home", "Analysis"]
 choice = st.sidebar.selectbox('Menu',menu)
 if(choice=='Home'):
+    st.title("Home Section")
     st.write("Click on the Arrow and scroll down to see the current parameters")
     st.json(data_json1,expanded=True)
     st.write(data_json2,expanded=False)
     st.write("Check out Analysis Section for more information")
 elif(choice=='Analysis'):
-    st.write("Analysis Section")
+    st.title("Analysis Section")
+    st.write("Data Table")
     dataframe=pd.DataFrame(data_json1['feeds'], columns=['entry_id','field1','field2','field3'])
     st.write(json_normalize(data_json1['feeds']))
     st.write(dataframe)
+    st.write("Line Chart for Voltage")
     st.altair_chart(alt.Chart(dataframe).mark_line(color='yellow').encode(x='entry_id:O',y='field1:Q').interactive(),use_container_width=True)
+    st.write("Line Chart for Current")
     st.altair_chart(alt.Chart(dataframe).mark_line(color='red').encode(x='entry_id:O',y='field2:Q').interactive(),use_container_width=True)
+    st.write("Line Chart for Power")
     st.altair_chart(alt.Chart(dataframe).mark_line(color='purple').encode(x='entry_id:O',y='field3:Q').interactive(),use_container_width=True)
     
+    st.write("Line Chart Comparison of Voltage, Current and Power")
     st.altair_chart(alt.layer(alt.Chart(dataframe).mark_line(color='blue').encode(x='entry_id:O',y='field1:Q').interactive(),
                               alt.Chart(dataframe).mark_line(color='yellow').encode(x='entry_id:O',y='field2:Q').interactive(),
                               alt.Chart(dataframe).mark_line(color='red').encode(x='entry_id:O',y='field3:Q').interactive()),use_container_width=True)
     
+    st.write("Area Chart for Voltage")
     st.altair_chart(alt.Chart(dataframe).mark_area(color='orange').encode(x='entry_id:O',y='field1:Q').interactive(),use_container_width=True)
+    st.write("Area Chart for Current")
     st.altair_chart(alt.Chart(dataframe).mark_area(color='magenta').encode(x='entry_id:O',y='field2:Q').interactive(),use_container_width=True)
+    st.write("Area Chart for Power")
     st.altair_chart(alt.Chart(dataframe).mark_area(color='yellow').encode(x='entry_id:O',y='field3:Q').interactive(),use_container_width=True)
     
+    st.write("Bar Chart for Voltage")
     st.altair_chart(alt.Chart(dataframe).mark_bar(color='red').encode(x='entry_id:O',y='field1:Q').interactive(),use_container_width=True)
+    st.write("Bar Chart for Current")
     st.altair_chart(alt.Chart(dataframe).mark_bar(color='green').encode(x='entry_id:O',y='field2:Q').interactive(),use_container_width=True)
+    st.write("Bar Chart for Power")
     st.altair_chart(alt.Chart(dataframe).mark_bar(color='cyan').encode(x='entry_id:O',y='field3:Q').interactive(),use_container_width=True)
